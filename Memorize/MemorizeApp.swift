14fab5659d2fbf0a386cbc09719cf3174358fc6e
Seth Micah King <sethmicahking@gmail.com>
2020-10-21 20:19:45 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by KNG001 on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
